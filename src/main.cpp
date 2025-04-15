40ee00f8eb3262a1f42754447a01601c0ff039a8
Kenichi Wakabayashi <kenichi@crssrds.jp>
2025-04-15 15:37:00 +0900

    ボタン操作追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <M5Stack.h>
 
+int cnt = 0; // カウント値
+
 void setup() {
   M5.begin();
   M5.Lcd.setTextSize(2);
@@ -8,5 +10,28 @@ void setup() {
 }
 
 void loop() {
+  M5.update();
+  // Aボタンが押されたら+1
+  if(M5.BtnA.wasPressed())
+  {
+    M5.Lcd.clear(); // 画面全体を消去
+    cnt++;
+  }
 
+  // Bボタンが押されたら-1
+  if(M5.BtnB.wasPressed())
+  {
+    M5.Lcd.clear(); // 画面全体を消去
+    cnt--;
+  }
+
+  // Cボタンが押されたら0
+  if(M5.BtnC.wasPressed())
+  {
+    M5.Lcd.clear(); // 画面全体を消去
+    cnt = 0;
+  }
+
+  delay(30);
+  M5.Lcd.drawString("CNT=" + String(cnt), 0, 40); // カウント値を表示
 } 
