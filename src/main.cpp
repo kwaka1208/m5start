4277a4fbdd53303f464f91dc6b1e9b78aae44a76
Kenichi Wakabayashi <kenichi@crssrds.jp>
2025-05-19 23:42:41 +0900

    M5Unifiedに変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
-#include <Arduino.h>
-#include <M5Stack.h>
+#include <M5Unified.h>
 
 int cnt = 0; // カウント値
 
 void setup() {
   M5.begin();
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.print("Hello World!");
+  M5.Display.setTextSize(2);
+  M5.Display.print("Hello World!");
 }
 
 void loop() {
@@ -14,24 +13,24 @@ void loop() {
   // Aボタンが押されたら+1
   if(M5.BtnA.wasPressed())
   {
-    M5.Lcd.clear(); // 画面全体を消去
+    M5.Display.clear(); // 画面全体を消去
     cnt++;
   }
 
   // Bボタンが押されたら-1
   if(M5.BtnB.wasPressed())
   {
-    M5.Lcd.clear(); // 画面全体を消去
+    M5.Display.clear(); // 画面全体を消去
     cnt--;
   }
 
   // Cボタンが押されたら0
   if(M5.BtnC.wasPressed())
   {
-    M5.Lcd.clear(); // 画面全体を消去
+    M5.Display.clear(); // 画面全体を消去
     cnt = 0;
   }
 
   delay(30);
-  M5.Lcd.drawString("CNT=" + String(cnt), 0, 40); // カウント値を表示
+  M5.Display.drawString("CNT=" + String(cnt), 0, 40); // カウント値を表示
 } 
